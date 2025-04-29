2dd099aa18574dc398e2962f085bd6342f88279c
krunal-lende <krunal.lende94@gmail.com>
2025-04-29 12:29:59 +0530

    Update Cart.jsx

--- src/components/body/Cart.jsx
+++ src/components/body/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+  import React, { useContext } from 'react'
 import styles from "./cart.module.css"
 import { AppContext } from '../../App'
 
@@ -41,12 +41,32 @@ const Cart = ({setShowCart,showCart}) => {
             }
                 </div>
             
-            <div className={styles.total}>
-            
-                <h2>TOTAL :- ${cart.reduce((acc,item)=>{return acc + item.price},0)}</h2>
-                <button onClick={()=>alert("Ready to Shipping ")}>Procid</button>
+                 { (cart.length === 0)?  <div className={styles.cart_empty}>Your cart is empty.</div>:""}
+                <div className={styles.card_contain}>
+                {
+                cart.map((prod)=>(<>
+                    <div className={styles.card_item}>
+                        <img src={prod.images[0]} alt={prod.title} />
+                        <div className={styles.info}>
+                        <p>{prod.title}</p>
+                        <h4>Price:- ${prod.price}</h4>
+                        </div>
+                        
+                        <button className={styles.remove}onClick={()=>removeHandlar(prod.id)} > Remove</button>
+                        
+                    </div>
+                    <hr />
+                    </>
+                ))
+            }
+                </div>
 
-            </div>
+                { (cart.length !== 0)?  <div className={styles.total}>
+            
+            <h2>TOTAL :- ${cart.reduce((acc,item)=>{return acc + item.price},0)}</h2>
+            <button onClick={()=>alert("Ready to Shipping ")}>Procid</button>
+
+        </div>:""}
 
         </div>
 
